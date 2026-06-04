2f8fe9c2a341d02afd116bc5c499e0ef8f2653a4
tony <kangwang@doodlemobile.com>
2026-06-04 16:14:32 +0800

    学习些布局

--- app/src/main/java/com/tony/accountapp/MainActivity.java
+++ app/src/main/java/com/tony/accountapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.tony.accountapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideSystemUI();
     }
 }
