3f5643c74202bb6a4664f25d0ebfdc2a9fd173cd
tony <kangwang@doodlemobile.com>
2026-06-08 10:28:31 +0800

    支持的布局比较少

--- app/src/main/java/com/tony/accountapp/TestWidgetProvider.java
+++ app/src/main/java/com/tony/accountapp/TestWidgetProvider.java
@@ -1,8 +1,19 @@
 package com.tony.accountapp;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
 
 public class TestWidgetProvider extends AppWidgetProvider {
 
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		for (int appWidgetId : appWidgetIds) {
+			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.test_widget);
+			appWidgetManager.updateAppWidget(appWidgetId, views);
+		}
+	}
 
 }
